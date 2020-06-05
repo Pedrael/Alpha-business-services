15b48e602cb86b84e612bec59e143a78e6de48ab
Pedrael <mihail_krivic@mail.ru>
2020-06-05 15:48:15 +0300

    added carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,25 @@
+import 'owl.carousel';
 
+$(document).ready(function() {
+  var tropheys = $('#tropheys-carousel')
+  tropheys.owlCarousel({
+    loop: false,
+    rewind: true,
+    items: 3,
+    dots: false,
+    autoplay: true,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        1200: {
+            items: 2
+        },
+        1900: {
+            items: 3
+        }
+    }
+  })
+});
