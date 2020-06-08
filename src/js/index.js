76b0a700abe145cf487fb7c15e325f4d13973586
Pedrael <mihail_krivic@mail.ru>
2020-06-08 19:10:33 +0300

    header and other layout stuff

--- src/js/index.js
+++ src/js/index.js
@@ -22,4 +22,31 @@ $(document).ready(function() {
         }
     }
   })
+
+  var headerCarousel = $('#header-carousel')
+  headerCarousel.owlCarousel({
+    loop: false,
+    rewind: true,
+    items: 3,
+    dots: false,
+    autoplay: false,
+    margin: 0,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        700: {
+            items: 2
+        },
+        1200: {
+            items: 3
+        }
+    }
+  })
+
+  var header = document.getElementById('header')
+  header.addEventListener('click', () => {
+    header.classList.toggle('menu-hidden')
+  })
 });
