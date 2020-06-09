05c5ff5d6ffc4af676c8db199bb59e701b827263
Pedrael <mihail_krivic@mail.ru>
2020-06-09 13:56:54 +0300

    fixes

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,12 @@
 import 'owl.carousel';
 import App from './middleware.js'
 import Mail from './mail.js'
+import Header from './header.js'
 import './jquery.maskedinput.js'
 
 App()
 Mail()
+Header()
 
 jQuery(function($) {
    $("#phone").mask("+7-999-999-9999", {autoclear: false});
